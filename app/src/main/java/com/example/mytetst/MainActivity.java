cbae89f11923e38e5ce59d936302058ec2a0f6bd
Stf <s.chotard@panoptes-fr.com>
2022-09-23 15:31:17 +0200

    gradle

--- app/src/main/java/com/example/mytetst/MainActivity.java
+++ app/src/main/java/com/example/mytetst/MainActivity.java
@@ -2,23 +2,34 @@ package com.example.mytetst;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import com.example.mytetst.arrivage.GetArrivage;
+
+import java.io.File;
+import java.net.URI;
+
 public class MainActivity extends AppCompatActivity {
-    private  static int RIGHTS = 0 ;
-    private LinearLayout arr;
+    @SuppressWarnings("FieldCanBeLocal")
+    private  static int RIGHTS ;
+     LinearLayout arr;
     private LinearLayout mouv;
+    private Button arrivage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
          arr = findViewById(R.id.line1);
+         arrivage = findViewById(R.id.arrivage);
          mouv = findViewById(R.id.line2);
 
         GetStatut(RIGHTS);
+
 
     }
 
@@ -30,6 +41,11 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case 2 :
                 mouv.setVisibility(View.GONE);
+                arrivage.setOnClickListener(v -> {
+                    Intent i = new Intent(getApplication(), GetArrivage.class);
+                    startActivity(i);
+
+                });
 
                 break;
 
---
+++ app/src/main/java/com/example/mytetst/arrivage/GetArrivage.java
@@ -0,0 +1,22 @@
+package com.example.mytetst.arrivage;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mytetst.R;
+
+public class GetArrivage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.arrivage);
+
+    }
+
+
+
+
+
+}
